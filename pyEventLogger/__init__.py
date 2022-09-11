e927d53787d3e30bb6cce9396e254ea8a9caa5d3
Siddhesh Chavan <70252606+Sid72020123@users.noreply.github.com>
2022-09-11 15:51:45 +0530

    Update __init__.py

--- pyEventLogger/__init__.py
+++ pyEventLogger/__init__.py
@@ -1,5 +1,5 @@
 __name__ = "pyEventLogger"
-__version__ = "1.0"
+__version__ = "1.1"
 __documentation__ = "https://github.com/Sid72020123/pyLogger"
 __developer__ = "Siddhesh Chavan"
 __doc__ = f"""
@@ -15,6 +15,7 @@ Change Log:
     27/01/2022(v0.8) - Added features to error and critical logs
     28/01/2022(v0.9) - Added doc-strings to code
     29/01/2022(v1.0) - First Release and changed some things!
+    11/09/2022(v1.1) -  Added some new things! Thanks to Quantum-Codes
 """
 
 from pyEventLogger.pyEventLogger import pyLogger
